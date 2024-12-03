ad72bf47b436dcadc48282b4500f9042ac896379
p4b10z <fp902405@gmail.com>
2024-12-03 13:40:11 -0300

    atualizando capitulo 11

--- src/Capitulo_11/DivideByZeroNoExceptionHandling.java
+++ src/Capitulo_11/DivideByZeroNoExceptionHandling.java
@@ -1,23 +1,42 @@
 package Capitulo_11;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DivideByZeroNoExceptionHandling {
-
-	public static int quotient(int numerador, int denominador) {
+	public static int quotient(int numerador, int denominador)
+	     throws ArithmeticException 
+	{
 		return numerador/ denominador ; 
 	}
 	public static void main(String[]args) {
-		Scanner input = new Scanner(System.in); 
+		Scanner input = new Scanner(System.in);
+		boolean continueLopp = true ; //determina se são necessarias mais entradas
 		
-		System.out.println("entre com o numerador ");
-	    int numerador = input.nextInt();
-	    		
-	    System.out.println("entre com o denominador");
-	    int denominador = input.nextInt();
-	    
-	    int resultado = quotient(numerador, denominador);
-	    
-	    System.out.printf("%nO Resultado é : %d /%d = %d%n", numerador, denominador, resultado);
+		do {
+			try {//lê os dois numeros e calcula o quociente
+				System.out.println("adicione o primeiro numero");
+				int numerador = input.nextInt();
+				System.out.println("adicione o segundo numero");
+				int denominador = input.nextInt();
+				
+				int result = quotient(numerador, denominador);
+				System.out.printf("%n%d/%d = %d", numerador, denominador, result);
+				continueLopp = false ; 	
+				
+			}catch (InputMismatchException inputMIssMachExcepition) {
+			//faz a analise para que exista uma entrada de um valor invalido 
+			System.out.printf("%nexeption: %s%n", 
+					inputMIssMachExcepition);
+			input.nextLine(); // para descartar a proxima linha 
+			System.out.println("caracteres invalidos por favor tente novamente");
+			}
+			catch(ArithmeticException aritimeticalEeption) {
+				System.out.printf("%nException : %s%n", aritimeticalEeption);
+				input.nextLine(); // para descartar a proxima linha novamente 
+				System.out.println("zero é um numero invalido, por favor tente novamente");
+			}
+		}
+		while(continueLopp);
+		
 	}
 }
