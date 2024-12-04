6b2b11d27c68b26b1f3e0c6f58e82a4873b73097
p4b10z <fp902405@gmail.com>
2024-12-03 23:20:09 -0300

    b

--- src/Capitulo_11/UsingExceptio.java
+++ src/Capitulo_11/UsingExceptio.java
@@ -1,5 +1,33 @@
 package Capitulo_11;
 
 public class UsingExceptio {
+    public static void main(String[] args) {
+        try {
+            throwException();
+        } catch (Exception exception) { // Captura a exceção lançada por throwException
+            System.err.println("Exception handled in main");
+        }
+        // Método `doesNotThrowException` implementado corretamente
+        doesNotThrowException();
+    }
 
+    // Demonstra try-catch-finally
+    public static void throwException() throws Exception {
+        try {
+            System.out.println("Method throwException");
+            throw new Exception(); // Gera uma exceção
+        } catch (Exception exception) {
+            System.err.println("Exception handled in method throwException");
+            throw exception; // Relança a exceção
+        } finally { // Executa independente do try-catch
+            System.err.println("finally executed in throwException");
+        }
+    }
+    public static void doesNotThrowException() {
+        try {
+            System.out.println("Method doesNotThrowException");
+        } finally {
+            System.err.println("finally executed in doesNotThrowException");
+        }
+    }
 }
